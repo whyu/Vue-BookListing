1d0489665fa38a4c92542dd227746cf5ec0de68e
Wesley Yu <wesley.yu@harley-davidson.com>
2020-07-14 10:23:04 -0400

    added BookList component, updated imports

---
+++ src/components/BookList.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'BookList',
+  data: function() {
+    return { 'title': 'All Books' }
+  }
+}
+</script>
+
+<style>
+
+h1 {
+  font-weight: normal;
+}
+
+h2 {
+  font-weight: normal;
+}
+
+</style>
